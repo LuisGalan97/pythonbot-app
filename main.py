a6bb1b2ea7acbe335ab3e1555c149c04277844ce
Luis Galan <miguelgalan97@gmail.com>
2024-01-07 00:35:49 -0500

    CREATED main.py UPDATED .gitignore
    Creacion del programa principal con una rutina de funcionamiento basica.
    Modificacion del gitignore para ignorar el archivo de configuracion en
    el repositorio.

---
+++ main.py
@@ -0,0 +1,26 @@
+# This example requires the 'message_content' intent.
+import sys
+sys.path.insert(1, './Config')
+from config import Config
+
+import discord
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print(f'We have logged in as {client.user}')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('$comando'):
+        await message.channel.send('Respuesta del bot en discord')
+
+client.run(Config.TOKEN)
+#Este token es valido unicamente para el servidor Omega-xis
