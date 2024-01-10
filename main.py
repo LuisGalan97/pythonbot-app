fe4d63d172068b2e39828927d78b7d6907c58d39
Luis Galan <miguelgalan97@gmail.com>
2024-01-10 03:58:28 -0500

    UPDATED main.py
    Pruebas de funcionalidad

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 # This example requires the 'message_content' intent.
 import sys
+import os
 sys.path.insert(1, './Config')
 from config import Config
 import discord
@@ -17,6 +18,21 @@ async def on_ready():
 async def on_message(message):
     if message.author == client.user:
         return
+    
+    if message.content.startswith('$enviar_archivo'):
+        # Crear un archivo de ejemplo
+        contenido_archivo = "Contenido del archivo de ejemplo."
+        with open("archivo_ejemplo.txt", "w", encoding="utf-8") as archivo:
+            archivo.write(contenido_archivo)
+
+        # Crear un objeto de tipo discord.File con el archivo
+        archivo_discord = discord.File("archivo_ejemplo.txt")
+
+        # Enviar el archivo al canal donde se recibió el mensaje
+        await message.channel.send(file=archivo_discord)
+
+        # Eliminar el archivo local (opcional)
+        os.remove("archivo_ejemplo.txt")
 
     if message.content.startswith('$comando'):
         await message.channel.send('Respuesta del bot en discord')
