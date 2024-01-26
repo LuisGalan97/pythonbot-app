22e1bffa1dafa8fd5d85e85c927da23e4f6f39f6
Luis Galan <miguelgalan97@gmail.com>
2024-01-26 02:00:07 -0500

    UPDATED Test/TestAssist/default.py
    Modificaciones de test.

--- Test/TestAssist/default.py
+++ Test/TestAssist/default.py
@@ -8,28 +8,37 @@ from collections import namedtuple
 Message = namedtuple('Message', ['author', 'content'])
 Client = namedtuple('Client', ['user'])
 app = AppHandler()
-idTest = None
 
 @pytest.mark.asyncio
-async def testAddAssist(capfd):  
-  message = Message(author="test", content="$addAssist [Avalonicus, defprismaganada, 25-01-2024]")
+async def testCRUDAssist(capfd):  
+  #----------------------------Create---------------------------------------
+  message = Message(author="test", content="$addAssist "\
+                    "[Avalonicus, defprismaganada, 25-01-2024]")
   client = Client(user="test")
   hdlr = MessageHandler(message, client, True)
-  await hdlr.inMsg()
-  await hdlr.contMsg("addAssist", app.setData, Helpers.setStruct("asistencia"))
+  await hdlr.contMsg("addAssist", app.setData, 
+                     Helpers.setStruct("asistencia"))
   out, _ = capfd.readouterr()
   idTest = out[out.find("**_ID_** '"):]
   idTest = idTest[idTest.find("'")+1:idTest.find("'.")]
   assert f"**_ID_** '{idTest}'" in out
-
-@pytest.mark.asyncio
-async def testDelAssist(capfd):  
-  message = Message(author="test", content=f"$delAssist:id [{idTest}]")
+  #--------------------------Update:id--------------------------------------
+  message = Message(author="test", content="$updAssist:id "\
+                    f"[{idTest}, Avalonicus, defprismaganada, 25-01-2024]")
   client = Client(user="test")
   hdlr = MessageHandler(message, client, True)
-  await hdlr.inMsg()
-  await hdlr.contMsg("delAssist:id", app.deleteData, Helpers.delStruct("asistencia", "id"))
+  await hdlr.contMsg("updAssist:id", app.updateData, 
+                     Helpers.updStruct("asistencia", "id"))
   out, _ = capfd.readouterr()
-  assert "exito" in out
+  assert "actualizado con exito" in out or "actualizada con exito" in out
+    #--------------------------Delete:id--------------------------------------
+  message = Message(author="test", content="$delAssist:id "\
+                    f"[{idTest}]")
+  client = Client(user="test")
+  hdlr = MessageHandler(message, client, True)
+  await hdlr.contMsg("delAssist:id", app.deleteData, 
+                     Helpers.delStruct("asistencia", "id"))
+  out, _ = capfd.readouterr()
+  assert "eliminado con exito" in out or "eliminada con exito" in out
 
 
