a4c3b28c0a92e53d4563a51600d732806b73615f
Luis Galan <miguelgalan97@gmail.com>
2024-01-26 00:53:09 -0500

    UPDATED Test/test.py
    Correccion de error en adicion al path.

--- Test/test.py
+++ Test/test.py
@@ -1,10 +1,10 @@
-from TestHelp.testHelp import *
-from TestAssist.testAssist import *
-from TestEvent.testEvent import *
-from TestMember.testMember import *
 import os
 import sys
 dir = os.path.dirname(os.path.abspath(__file__))
 dir = os.path.dirname(dir)
 sys.path.insert(1, dir)
+from TestHelp.testHelp import *
+from TestAssist.testAssist import *
+from TestEvent.testEvent import *
+from TestMember.testMember import *
 from TestRange.testRange import *
