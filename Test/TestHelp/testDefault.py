83a0294f2637d3f6360ac9d72aba71c04446eee4
Luis Galan <miguelgalan97@gmail.com>
2024-01-28 07:16:22 -0500

    UPDATED Test/TestHelp/testDefault.py
    Ajustes de test.

--- Test/TestHelp/testDefault.py
+++ Test/TestHelp/testDefault.py
@@ -28,6 +28,28 @@ async def test_help(capfd):
   assert "**___Guia de usuario de Avalon-bot___**" in out
   assert "Bienvenido/a a la guia de usuario del "\
          "bot de **⚜Avalon⚜** para discord. " in out
+  assert "En esta seccion realizaremos una breve introduccion, "\
+         "de las funciones principales que dispone **_Avalon-bot_**, "\
+         "la estructura de la informacion con la que trabaja, "\
+         "y como podemos utilizarlo para obtener importantes beneficios, "\
+         "en el manejo y gestion de los datos asociados con la alianza."
+  assert "**_¿Que es Avalon-bot?_**"
+  assert "**_Avalon-bot_** es una herramienta pensada y diseñada "\
+         "para facilitar al usuario la gestion de los "\
+         "datos asociados con la alianza **⚜Avalon⚜**, "\
+         "mediante la comprension de la estructura "\
+         "de la informacion planteada para este proposito, "\
+         "y la correcta utilizacion de los comandos dispuestos "\
+         "para poder acceder y manipular dicha informacion. "\
+         "Dicho esto, con Avalon-bot podras generar un "\
+         "seguimiento automatizado y estructurado, "\
+         "de datos relacionados con los ___integrantes___, "\
+         "las ___asistencias___, los ___rangos___, "\
+         "y los ___eventos___ establecidos en la alianza, "\
+         "en conjunto con la capacidad de generar informes "\
+         "personalizados en excel, gracias a la implementacion "\
+         "de bases de datos SQL."
+  assert "**_¿Como se encuentra estructurada la informacion?_**"
   
 @pytest.mark.asyncio
 async def test_helpDiagram(capfd):
