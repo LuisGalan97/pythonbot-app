24c171c50fe4cc1c13f0a182eae346a8b40c005c
Luis Galan <miguelgalan97@gmail.com>
2024-01-29 22:22:16 -0500

    UPDATED Test/TestHelp/testDefault.py
    Ajustes de test.

--- Test/TestHelp/testDefault.py
+++ Test/TestHelp/testDefault.py
@@ -527,7 +527,7 @@ async def test_helpRange(capfd):
            "registros en el canal de discord, sin embargo, tambien "\
            "pueden ser impresos dentro de una hoja de excel, si "\
            "despues del comando se especifica el parametro **> e**.\n" in out
-    assert "- **$listRange > e**   ->   Lista en una hoja de excel"\
+    assert "- **$listRange > e**   ->   Lista en una hoja de excel "\
            "todos los ___rangos___.\n" in out
     assert "- **$listRange:id [_ID_]** **> e**   ->   Lista "\
            "en una hoja de excel el ___rango___ "\
