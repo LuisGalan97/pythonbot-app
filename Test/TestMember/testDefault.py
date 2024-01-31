7382f4ec96e3b6cada6293535d006a0435143dfc
Luis Galan <miguelgalan97@gmail.com>
2024-01-31 12:35:05 -0500

    UPDATED Test/TestMember/testDefault.py
    Ajustes de test.

--- Test/TestMember/testDefault.py
+++ Test/TestMember/testDefault.py
@@ -45,8 +45,6 @@ async def test_addMember_exist(capfd):
     await hdlr.contMsg("addMember", app.setData,
                        Helpers.setStruct("integrante"))
     out, _ = capfd.readouterr()
-    idTest = out[out.find("**_ID_** '"):]
-    testData["id"] = idTest[idTest.find("'")+1:idTest.find("'.")]
     assert f"El ___integrante___ de **_Nombre_** "\
            f"\'{testData['namecreate']}\' "\
             "ya se encuentra en la base de datos.\n" in out
