080c7219fd978b75f1a12bf1ab6e61a3523e9d7f
Luis Galan <miguelgalan97@gmail.com>
2024-02-05 06:35:55 -0500

    UPDATED Test/TestConstraints/testDefault.py
    Ajustes de test.

--- Test/TestConstraints/testDefault.py
+++ Test/TestConstraints/testDefault.py
@@ -15,6 +15,13 @@ testData = {
     "mem_update" : "TestMemberUpdated",
     "mem_datecreate" : "25-01-2100",
     "mem_dateupdate" : "26-01-2100",
+    "ev_id" : "",
+    "ev_create" : "TestEventCreated",
+    "ev_update" : "TestEventUpdated",
+    "ev_point" : 5,
+    "ev_des" : "Descripción creada",
+    "asist_id" : "",
+    "asist_date" : "27-01-2100"
 }
 
 Message = namedtuple('Message', ['author', 'content'])
@@ -82,6 +89,36 @@ async def testConstraintsDefault_listMemberId(capfd):
     assert f"{testData['ran_create']}" in out
     assert f"{testData['mem_datecreate'].replace('-','/')}" in out
     assert "Ninguno" in out
+
+@pytest.mark.asyncio
+async def testConstraintsDefault_addEvent(capfd):
+    command = f"$addEvent [{testData['ev_create']}, "\
+              f"{testData['ev_point']}, {testData['ev_des']}]"
+    message = Message(author="test", content=command)
+    client = Client(user="test")
+    hdlr = MessageHandler(message, client, True)
+    await hdlr.contMsg("addEvent", app.setData,
+                       Helpers.setStruct("event"))
+    out, _ = capfd.readouterr()
+    idTest = out[out.find("**_ID_** '"):]
+    testData["ev_id"] = idTest[idTest.find("'")+1:idTest.find("'.")]
+    assert "El ___evento___ ha sido creado con exito sobre el "\
+          f"**_ID_** \'{testData['ev_id']}\'.\n" in out
+
+@pytest.mark.asyncio
+async def testConstraintsDefault_listEventId(capfd):
+    command = f"$listEvent:id [{testData['ev_id']}]"
+    message = Message(author="test", content=command)
+    client = Client(user="test")
+    hdlr = MessageHandler(message, client, True)
+    await hdlr.dFMsg("listEvent:id", app.getDatas,
+                     Helpers.getStruct("event", ["id"]))
+    out, _ = capfd.readouterr()
+    assert "**___Eventos___** **___encontrados:___**\n" in out
+    assert f"{testData['ev_id']}" in out
+    assert f"{testData['ev_create']}" in out
+    assert f"{testData['ev_point']}" in out
+    assert f"{testData['ev_des']}" in out
 
 @pytest.mark.asyncio
 async def testConstraintsDefault_updRangeId(capfd):
