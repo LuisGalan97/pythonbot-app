3326dae0384b7b5fa635e8ab3da655c6e9727a59
Luis Galan <miguelgalan97@gmail.com>
2024-02-14 17:32:37 -0500

    UPDATED Test/TestPointMember/testDefault.py
    Ajustes de test.

--- Test/TestPointMember/testDefault.py
+++ Test/TestPointMember/testDefault.py
@@ -329,7 +329,40 @@ async def testPointMemberDefault_addAssist_12(capfd):
           f"**_ID_** \'{testData['idassist_12']}\'.\n" in out
 
 @pytest.mark.asyncio
-async def testPointMemberDefault_listPointMember(capfd):
+async def testPointMemberDefault_listPointMember_partial(capfd):
+    commands = [f"$listPointMember[{testData['assistdate_1']},"\
+                f"{testData['assistdate_6']}]",
+                f"$listPointMember [{testData['assistdate_1']}, "\
+                f"{testData['assistdate_6']}]",
+                f"$listPointMember [ {testData['assistdate_1']} , "\
+                f"{testData['assistdate_6']} ] ",
+                f"$listPointMember [ {testData['assistdate_1']} , "\
+                f"{testData['assistdate_6']} ]FILL",
+                f"$listPointMember [ {testData['assistdate_1']} , "\
+                f"{testData['assistdate_6']} ] FILL"]
+    for command in commands:
+        channel = Channel(name=name)
+        message = Message(author=author, content=command, channel=channel)
+        client = Client(user=user)
+        hdlr = MessageHandler(message, client, True)
+        await hdlr.dFMsg("listPointMember", app.getDatas,
+                     Helpers.getStruct("member",
+                                       ["assist_date_1",
+                                        "assist_date_2"]))
+        out, _ = capfd.readouterr()
+        assert "**___Integrantes___** **___encontrados:___**\n" in out
+        assert f"{testData['idmember']}" in out
+        assert f"{testData['memname']}" in out
+        assert f"{testData['ranname']}" in out
+        assert f"{testData['memdate'].replace('-','/')}" in out
+        assert "Ninguno" in out
+        totalPoints = (2*testData['evpoints_1'] +
+                       2*testData['evpoints_2'] +
+                       2*testData['evpoints_3'])
+        assert str(totalPoints) in out
+
+@pytest.mark.asyncio
+async def testPointMemberDefault_listPointMember_total(capfd):
     commands = [f"$listPointMember[{testData['assistdate_1']},"\
                 f"{testData['assistdate_12']}]",
                 f"$listPointMember [{testData['assistdate_1']}, "\
