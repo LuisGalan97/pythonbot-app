397d6bae1c9c7dbab64226928e4800e8b2ff7927
Luis Galan <miguelgalan97@gmail.com>
2024-02-16 19:11:05 -0500

    UPDATED Helpers/helpers.py
    Ajustes de configuracion.

--- Helpers/helpers.py
+++ Helpers/helpers.py
@@ -6,7 +6,7 @@ class Helpers:
         user = str(author)
         channel = str(nameChannel)
         adminUser = ["test", "lia7624", "evermell", "omegaxis_"]
-        adminChannel = ["test", "👑consejo"]
+        adminChannel = ["test", "🔏alimentacion-bot"]
         access = {}
         access['hello'] = {
             "user" : adminUser + [],
