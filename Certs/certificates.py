a818db9112a8e94ce8ecea666aa8953cd5897821
Luis Galan <miguelgalan97@gmail.com>
2024-02-17 17:29:08 -0500

    CREATED Certs/Commands/template.json CREATED Certs/certificates.py
    Creacion del modulo certificados, diseñado
    para gestionar los permisos de los comandos.

---
+++ Certs/certificates.py
@@ -0,0 +1,39 @@
+import os
+dir = os.path.dirname(os.path.abspath(__file__))
+import json
+
+class Certificates:
+    def __init__(self):
+        adminUser = "test, lia7624, evermell, omegaxis_"
+        adminChannel = "general, test, 🔏alimentacion-bot"
+        self.createRule("hello", adminUser, adminChannel)
+        self.createRule("help", adminUser, adminChannel)
+        self.createRule("addAssist", adminUser, adminChannel)
+        self.createRule("updAssist:id", adminUser, adminChannel)
+        self.createRule("delAssist:id", adminUser, adminChannel)
+        self.createRule("listAssist", adminUser, adminChannel)
+        self.createRule("listAssist:id", adminUser, adminChannel)
+        self.createRule("listAssist:member", adminUser, adminChannel)
+
+    def createRule(self, command, user, channel):
+        command = command.replace(":", "_")
+        filepath = f"{dir}/{command}"
+        if os.path.exists(filepath):
+            archivos = os.listdir(filepath)
+            print(archivos)
+        else:
+            os.makedirs(os.path.dirname(filepath), exist_ok=True)
+
+        '''
+        rule = 1
+        if not os.path.exists(filepath):
+            
+            with open(f'{dir}/{command}/archivo.json', "w") as file_json:
+                json.dumps({
+                    "user" : [item.strip() for item in user.split(',')],
+                    "channel" : [item.strip() for item in channel.split(',')]
+                }, file_json, ensure_ascii=False, indent=4)
+            print(f"-> Created rule '/{command}/{rule}.json'!")
+        else: 
+            print(f"-> Rule '/{command}/{rule}.json' already exist...")
+        '''
