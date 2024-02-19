45e50142b1ff7b937075fd7f5495a8d58adbd7e8
Luis Galan <miguelgalan97@gmail.com>
2024-02-19 15:59:45 -0500

    UPDATED Controllers/memberController.py
    Ajustes de controladores members.

--- Controllers/memberController.py
+++ Controllers/memberController.py
@@ -32,13 +32,15 @@ class MemberController:
         else:
             return members
 
-    def createMember(self, name, range_id, date):
+    def createMember(self, name, range_id, date, principal_id = None):
+        print(principal_id)
         range = RangeModel(range_id)
         member = MemberModel(None, name, range, None, date)
         result = self.__service.insert(member)
         return result
 
-    def updateMember(self, id, name, range_id, date):
+    def updateMember(self, id, name, range_id, date, principal_id = None):
+        print(principal_id)
         range = RangeModel(range_id)
         member = MemberModel(id, name, range, None, None, date)
         result = self.__service.update(member)
